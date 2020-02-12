9f6a0c6fdde7c5af48c3396ad3c66e005515b68d
yosepjeon <enekelx1@naver.com>
2020-02-12 19:32:58 +0900

    모던자바 인 액션 chapter 4.2 4.3 학습완료

--- ModernJavaInAction/chapter4/HighCaloriesNames.java
+++ ModernJavaInAction/chapter4/HighCaloriesNames.java
@@ -1,24 +1,27 @@
 package ModernJavaInAction.chapter4;
-
+import static ModernJavaInAction.chapter4.Dish.menu;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import static ModernJavaInAction.chapter4.Dish.menu;
-
 public class HighCaloriesNames {
 	public static void main(String[] args ) {
-		List<String> names = menu.stream()
-				.filter(dish -> {
-					System.out.println("filtering " + dish.getName());
-					return dish.getCalories() > 300;
-				})
-				.map(dish -> {
-					System.out.println("mapping " + dish.getName());
-					return dish.getName();
-				})
-				.limit(3)
+//		List<String> names = menu.stream()
+//				.filter(dish -> {
+//					System.out.println("filtering " + dish.getName());
+//					return dish.getCalories() > 300;
+//				})
+//				.map(dish -> {
+//					System.out.println("mapping " + dish.getName());
+//					return dish.getName();
+//				})
+//				.collect(Collectors.toList());
+		
+		List<Dish> highCalories = menu.stream()
+				.filter(dish -> dish.getCalories() > 300)
+				.map(dish -> dish)
 				.collect(Collectors.toList());
 		
-		System.out.println(names);
+		System.out.println(highCalories);
 	}
 }
