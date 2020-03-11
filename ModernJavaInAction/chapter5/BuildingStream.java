d19715a4fc41b146347224d08950b95daca03496
yosepjeon <enekelx1@naver.com>
2020-03-11 23:16:09 +0900

    모던 자바 인 액션 5.8.1~5.8.4 학습완료

---
+++ ModernJavaInAction/chapter5/BuildingStream.java
@@ -0,0 +1,26 @@
+package ModernJavaInAction.chapter5;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+public class BuildingStream {
+	public static void main(String[] args) throws IOException {
+		// Stream.of
+		Stream<String> stream = Stream.of("Java 8", "Lambdas", "In", "Action");
+		stream.map(String::toUpperCase).forEach(System.out::println);
+
+		//Stream.empty
+		Stream<String> emptyStream = Stream.empty();
+		
+		//file stream
+		long uniqueWords = Files.lines(Paths.get("src/ModernJavaInAction/chapter5/data.txt"), Charset.defaultCharset())
+		        .flatMap(line -> Arrays.stream(line.split(" ")))
+		        .distinct()
+		        .count();
+		System.out.println("There are " + uniqueWords + " unique words in data.txt");
+	}
+}
---
+++ ModernJavaInAction/chapter5/NumericStream.java
@@ -0,0 +1,53 @@
+package ModernJavaInAction.chapter5;
+
+import static ModernJavaInAction.chapter4.Dish.menu;
+import java.util.Arrays;
+
+import java.util.List;
+import java.util.OptionalInt;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+import ModernJavaInAction.chapter4.Dish;
+
+public class NumericStream {
+	public static void main(String[] args) {
+		List<Integer> numbers = Arrays.asList(3, 4, 5, 1, 2);
+
+		Arrays.stream(numbers.toArray()).forEach(System.out::println);
+
+		int calories = menu.stream().mapToInt(Dish::getCalories).sum();
+		System.out.println("Number of calories: " + calories);
+
+		// max와 OptionalInt
+		OptionalInt maxCalories = menu.stream().mapToInt(Dish::getCalories).max();
+
+		int max;
+		if (maxCalories.isPresent()) {
+			max = maxCalories.getAsInt();
+		} else {
+			// 기본값을 선택할 수 있음
+			max = 1;
+		}
+		System.out.println(max);
+
+		// 숫자 범위
+		IntStream evenNumbers = IntStream.rangeClosed(1, 100).filter(n -> n % 2 == 0);
+		System.out.println(evenNumbers.count());
+
+		// 피타고라스 수
+		Stream<int[]> pythagoreanTriples = IntStream.rangeClosed(1, 100).boxed()
+				.flatMap(a -> IntStream.rangeClosed(a, 100)
+					.filter(b -> Math.sqrt(a * a + b * b) % 1 == 0).boxed()
+					.map(b -> new int[] { a, b, (int) Math.sqrt(a * a + b * b) })).limit(5);
+		pythagoreanTriples.forEach(t -> System.out.println(t[0] + ", " + t[1] + ", " + t[2]));
+		
+		Stream<int[]> pythagoreanTriples2 = IntStream.rangeClosed(1, 100).boxed()
+				.flatMap(a -> IntStream.rangeClosed(a, 100)
+					.mapToObj(b -> new double[]{a,b,Math.sqrt(a*a + b*b)})
+					.filter(t -> t[2] % 1 == 0))
+				.map(array -> Arrays.stream(array).mapToInt(a -> (int) a).toArray());
+		pythagoreanTriples2.forEach(t -> System.out.println(t[0] + ", " + t[1] + ", " + t[2]));
+		
+	}
+}
