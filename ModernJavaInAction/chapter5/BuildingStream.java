fcb059252d6cd960a95f9106fca250d90af4561d
yosepjeon <enekelx1@naver.com>
2020-03-15 23:34:58 +0900

    모던자바 인 액션 학습중..

--- ModernJavaInAction/chapter5/BuildingStream.java
+++ ModernJavaInAction/chapter5/BuildingStream.java
@@ -1,6 +1,7 @@
 package ModernJavaInAction.chapter5;
 
 import java.io.IOException;
+import java.math.BigInteger;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -13,15 +14,25 @@ public class BuildingStream {
 		Stream<String> stream = Stream.of("Java 8", "Lambdas", "In", "Action");
 		stream.map(String::toUpperCase).forEach(System.out::println);
 
-		//Stream.empty
+		// Stream.empty
 		Stream<String> emptyStream = Stream.empty();
-		
-		//file stream
+
+		// file stream
 		long uniqueWords = Files.lines(Paths.get("src/ModernJavaInAction/chapter5/data.txt"), Charset.defaultCharset())
-		        .flatMap(line -> Arrays.stream(line.split(" ")))
-		        .distinct()
-		        .count();
+				.flatMap(line -> Arrays.stream(line.split(" "))).distinct().count();
 		System.out.println("There are " + uniqueWords + " unique words in data.txt");
+		
+		// Stream.iterate
+		Stream.iterate(0, n -> n + 2).limit(10).forEach(System.out::println);
+		
+		// iterate를 이용한 피보나치
+		Stream.iterate(new BigInteger[]{BigInteger.ZERO,BigInteger.ONE}, t -> {
+			BigInteger temp;
+			temp = t[0].add(t[1]);
+			t[0] = t[1];
+			t[1] = temp;
+			return t;
+		}).limit(300).map(t -> t[0]).forEach(System.out::println);
 		
 		//
 	}
